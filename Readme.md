0b66d667e6dd0ee9876fa4a0e3ea79099110b68a
DecDuck <declan@deepcore.dev>
2022-09-20 17:15:38 +1000

    Inital Commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Spellbind
+Spellbind is a magical overhaul mod for the Sapiens video game. Now your Sapiens can become wizards and witches, and use magic to their advantage!
---
+++ modInfo.lua
@@ -0,0 +1,9 @@
+local modInfo = {
+    name = "Spellbind",
+    description = "A magical overhaul for Sapiens. Now Sapiens can be wizards and witches, and use magic to their advantage!",
+    type = "world",
+    developer = "DecDuck",
+    version = "0.0.1",
+    preview = "assets/preview.jpg",
+    website = "https://github.com/DecDuck/spellbind"
+}
